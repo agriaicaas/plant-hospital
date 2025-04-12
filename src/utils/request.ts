b613b6ed25bb13d8b34a827a7eabc3866badcaa3
agriaicaas <89518101+agriaicaas@users.noreply.github.com>
2025-04-12 11:50:22 +0800

    Update request.ts

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,9 +1,4 @@
 /*
- * @Author: 吴伟 15711376175@163.com
- * @Date: 2025-03-04 17:11:46
- * @LastEditors: 吴伟 15711376175@163.com
- * @LastEditTime: 2025-03-04 18:00:29
- * @FilePath: /ai/src/utils/request.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import axios from 'axios'
