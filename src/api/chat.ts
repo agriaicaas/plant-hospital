7808521ccca399adb3d941988138671cb668c023
agriaicaas <89518101+agriaicaas@users.noreply.github.com>
2025-04-12 11:48:04 +0800

    Update chat.ts

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -1,9 +1,5 @@
 /*
- * @Author: 吴伟 15711376175@163.com
- * @Date: 2025-03-04 17:11:53
- * @LastEditors: 吴伟 15711376175@163.com
- * @LastEditTime: 2025-04-10 20:14:47
- * @FilePath: /ai/src/api/chat.ts
+ 
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import request from '@/utils/request'
