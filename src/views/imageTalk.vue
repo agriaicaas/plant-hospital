18e0d5d1aa499271f0e040da01d9364e0bfd5b40
agriaicaas <89518101+agriaicaas@users.noreply.github.com>
2025-04-13 09:36:18 +0800

    Update imageTalk.vue

--- src/views/imageTalk.vue
+++ src/views/imageTalk.vue
@@ -1,9 +1,5 @@
 <!--
- * @Author: 吴伟 15711376175@163.com
- * @Date: 2025-03-06 09:52:29
- * @LastEditors: 吴伟 15711376175@163.com
- * @LastEditTime: 2025-03-19 18:10:56
- * @FilePath: /ai/src/views/imageTalk.vue
+ 
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
